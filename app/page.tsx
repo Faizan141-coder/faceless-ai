b3e990d9a2ef3bc36b45173846d0db41c21c60e5
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-08-02 00:04:56 +0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import {
   Select,
   SelectContent,
@@ -11,22 +14,89 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import Image from "next/image";
+import { useState } from "react";
 
 const images = [
-  { id: 1, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 2, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "Minecraft Parkour" },
-  { id: 3, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "Minecraft Night" },
-  { id: 4, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 5, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 6, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 7, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 8, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
-  { id: 9, src: "/ai-image.jpg", alt: "Photo by Drew Beamer", title: "AI Generated ✨" },
+  {
+    id: 1,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 2,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "Minecraft Parkour",
+  },
+  {
+    id: 3,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "Minecraft Night",
+  },
+  {
+    id: 4,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 5,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 6,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 7,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 8,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+  {
+    id: 9,
+    src: "/ai-image.jpg",
+    alt: "Photo by Drew Beamer",
+    title: "AI Generated ✨",
+  },
+];
+
+const terms = [
+  { id: 1, label: "MONTSERRAT (Default)" },
+  { id: 2, label: "THE BOLD FONT" },
+  { id: 3, label: "DEUTSCH" },
+  { id: 4, label: "ADDISON" },
+  { id: 5, label: "LEMON" },
+  { id: 6, label: "HOLTWOOD ONE" },
+  { id: 7, label: "HOLTWOOD ONE" },
+  { id: 8, label: "HOLTWOOD ONE" },
+  { id: 9, label: "HOLTWOOD ONE" },
+  { id: 10, label: "HOLTWOOD ONE" },
+  { id: 11, label: "HOLTWOOD ONE" },
+  { id: 12, label: "HOLTWOOD ONE" },
 ];
 
 export default function Home() {
+  const [selectedColor, setSelectedColor] = useState("#23a05b");
+
+  const handleColorChange = (event: any) => {
+    setSelectedColor(event.target.value);
+  };
+
   return (
-    <div>
+    <div className="pb-20">
       {/* Select Story Type */}
       <div className="flex flex-col pt-36 items-center justify-center">
         <h1 className="text-5xl">Select a Story Type</h1>
@@ -119,6 +189,61 @@ export default function Home() {
             </div>
           ))}
         </div>
+      </div>
+
+      {/* Fonts */}
+      <div className="flex flex-col pt-36 items-center justify-center">
+        <h1 className="text-5xl">Select your Fonts</h1>
+        <h2 className="mt-6 text-neutral-400 text-sm">
+          You can select multiple fonts to be used in your Series.
+        </h2>
+        <h2 className="text-neutral-400 text-sm">
+          Selected fonts will be chosen at random per video.
+        </h2>
+
+        <div className="mt-4">
+          <input
+            type="color"
+            id="color-picker"
+            value={selectedColor}
+            onChange={handleColorChange}
+            className="mr-2"
+          />
+          <label
+            htmlFor="color-picker"
+            className="font-bold leading-none text-xl"
+          >
+            Color
+          </label>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div className="grid grid-cols-2 gap-5 mt-10 items-center justify-center">
+            {terms.map((term) => (
+              <div key={term.id} className="flex items-center space-x-2">
+                <Checkbox id={`terms-${term.id}`} />
+                <label
+                  htmlFor={`terms-${term.id}`}
+                  className="text-sm font-extrabold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  style={{ color: selectedColor }}
+                >
+                  {term.label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Preview Series */}
+      <div className="flex flex-col pt-28 items-center justify-center">
+        <h1 className="text-5xl">Preview Your Series</h1>
+        <h2 className="mt-6 text-neutral-400 text-sm">
+          You can select multiple voices to be used in your Series.
+        </h2>
+        <h2 className="text-neutral-400 text-sm">
+          Selected voices will be chosen at random per video.
+        </h2>
       </div>
     </div>
   );
