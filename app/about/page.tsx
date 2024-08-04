61821e6f12ee01642654c3682043f7a096193a4e
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-08-04 14:47:45 +0500

    about section completed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen text-white p-8">
+      <div className="max-w-4xl">
+        <h1 className="text-2xl font-bold mb-8 -mt-14">ABOUT FACELESS.VIDEO</h1>
+        <p className="mb-4 text-sm">
+          Faceless.video was created out of necessity. We wanted to manage our
+          own Faceless channels, but when starting, found that it wasn&apos;t as
+          easy as we&apos;d thought.
+        </p>
+        <p className="mb-4 text-sm">
+          Even though we didn&apos;t have to be in the videos, it still required{" "}
+          <span className="font-bold italic">
+            finding stories every day, finding video clips, learning how to edit
+            videos, exporting, and uploading
+          </span>{" "}
+          - which could take upwards of{" "}
+          <span className="font-bold">3 hours per video to complete</span>.
+        </p>
+        <p className="mb-4 text-sm">
+          Then we had the idea - what if we could make a tool that automates
+          Faceless channels for us. It would{" "}
+          <span className="font-bold text-green-500">
+            find stories, create videos, then post it to our channels every day
+          </span>
+          .
+        </p>
+        <p className="mb-4 text-sm">
+          Faceless.video started out as a tool just for us, but became a service
+          for others. We wanted to share the power of owning Faceless channels,
+          making it as easy as clicking a button.
+        </p>
+        <p className="mb-4 text-green-500 font-bold text-sm">
+          Faceless.video serves as your content creation team, working 24/7 to
+          create and post videos for you daily.
+        </p>
+        <p className="mb-4 text-sm">
+          Growing a channel and monetizing an audience has never been easier. We
+          hope you enjoy Faceless.video as much as we enjoyed making it.
+        </p>
+        <div className="flex flex-col items-center justify-center mt-7">
+          <Button className="bg-[#4bf05b] uppercase text-black font-semibold px-6 hover:bg-[#3fc34d]">
+            TRY IT OUT
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,32 @@
 import Image from "next/image";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="border-b w-full py-4 px-5 flex justify-between">
-      <div className="flex items-center space-x-2">
-        <Image
-          src="/faceless_logo.png"
-          width={35}
-          height={35}
-          alt="Faceless Logo"
-        />
-        <h1 className="text-lg">faceless.video</h1>
-      </div>
+      <Link href={'/'}>
+        <div className="flex items-center space-x-2">
+          <Image
+            src="/faceless_logo.png"
+            width={35}
+            height={35}
+            alt="Faceless Logo"
+          />
+          <h1 className="text-lg">faceless.video</h1>
+        </div>
+      </Link>
       <div className="flex items-center space-x-8 text-lg">
-        <h1 className="font-bold text-[#a6a8ab] hover:underline">Pricing</h1>
-        <h1 className="font-bold text-[#a6a8ab] hover:underline">About</h1>
-        <h1 className="font-bold text-[#a6a8ab] hover:underline">Sign in</h1>
+        <Link href={"/pricing"}>
+          <h1 className="font-bold text-[#a6a8ab] hover:underline">Pricing</h1>
+        </Link>
+        <Link href={"/about"}>
+          <h1 className="font-bold text-[#a6a8ab] hover:underline">About</h1>
+        </Link>
+        <Link href={"/sign-in"}>
+          <h1 className="font-bold text-[#a6a8ab] hover:underline">Sign in</h1>
+        </Link>
         <Button className="bg-[#4bf05b] uppercase text-black font-semibold px-6 hover:bg-[#3fc34d]">
           Free Trial
         </Button>
